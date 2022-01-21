5539022c90c460b4fea2a8fbcb7e5c05acad9971
shahidahimran <45367080+shahidahimran@users.noreply.github.com>
2022-01-21 22:58:24 +0800

    Made changes.

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Making changes to code by adding this line
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
